dc00da565d5158f5d500f099b24d9e4a25f6f55e
Andy Fields <andyfieldsy89@gmail.com>
2018-09-19 09:52:09 -0700

    Comments added and refactored a little

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ const app = express();
 // Tells express to use 'cors' for cross-origin resource sharing
 app.use(cors());
 
+// Allows us to use the .env file
 require('dotenv').config();
 
+// assigns the PORT variable to equal the port declared in the .env file for our local server.  It also allows heroku to assign it's own port number.
 const PORT = process.env.PORT;
 
-app.get('/location', getGoogleLocation);
+// The following app.get() will call the correct helper function to retrieve the API information.
+app.get('/location', getGoogleLocation); //google API
 
 
+// Tells the server to listen to the PORT, and console.logs to tell us it's on.
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+// HELPER FUNCTIONS BELOW //
+// Google helper function refactored prior to lab start.
 function getGoogleLocation(request, response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GOOGLE_API_KEY}`;
   return superagent.get(url)
